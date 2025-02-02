f0b7c493aea65b2fece53a27169bf718cabf89da
okankukul <okankukull@gmail.com>
2025-02-03 00:03:58 +0300

    Initial Commit

---
+++ PhotoGallerry/PhotoGallerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGallerryApp.swift
+//  PhotoGallerry
+//
+//  Created by Okan Kukul on 3.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGallerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
